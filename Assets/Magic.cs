2f592b1b43e2f1b714c69a16d82a6f814814b996
Andrew Lewis <andrew12lewis@gmail.com>
2020-10-06 21:16:33 -0700

    First commit
    This adds some starter code

---
+++ Assets/Magic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject fireball;
+    public Transform Gem;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(fireball, Gem.position, Gem.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * Gem.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+    }
+}
